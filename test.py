a13e4770e95dcc915ef1536d7005a45ffd03dd6b
matt <pagey101@hotmail.co.uk>
2021-07-16 12:36:16 +0100

    testing

--- test.py
+++ test.py
@@ -6,17 +6,18 @@ class TestCrawler(unittest.TestCase):
     def test_empty_crawl(self):
         spider = Spider(exclusive_domain=True)
         results = spider.crawl()
+        results = results[0]
+        self.assertTrue(len(results)==1)
         for link, links in results.items():
             self.assertTrue(len(links) == 0)
 
-    def test_crawl(self):
+    def test_error_response(self):
         spider = Spider(domain='https://www.york.ac.uk/teaching/cws/wws/webpage1.html', exclusive_domain=True)
         results = spider.crawl()
         self.assertTrue(len(results)==1)
-        results = next(iter(results.values()))
-        self.assertTrue('#Main-Content' in results)
-        self.assertTrue('#Mobile-Search' in results)
-        self.assertTrue('#Main-Navigation' in results)
+        results = results[0]
+        for link, links in results.items():
+            self.assertTrue(links == 'got http error response 404')
 
 if __name__ == '__main__':
     unittest.main()
